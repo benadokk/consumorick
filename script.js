0c48453760f3d683b885ea239710b1cea6d4b103
benadokk <ber.oliveira08@gmail.com>
2025-08-19 11:25:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const pageInput =document.getElementById("pageInput")
+const searcBtn =document.getElementById("searchBtn")
+const resultsDiv = document.getElementById("results")
+
+async function fetchCharacters(page){
+    resultsDiv.innerHTML = "<p>Carregando..</p>"
+
+try {
+    const response = await fetch(`https://rickandmortyapi.com/api/character`)
+    const data = await response.json()
+    console.log(data)
+} catch (error) {
+    resultsDiv.innerHTML="<p>Erro ao buscar personagens!</p>"
+    
+}
+}
+
+
+fetchCharacters(1) 
